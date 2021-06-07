634f238fbb24f4e226dc6cf4da77edda7e95dfd4
Ivan Martynov <ivan.a.martynov@gmail.com>
2021-06-07 07:03:11 +0300

    removing unnecessary code

--- src/utils/colors/ColorHelper.ts
+++
@@ -1,7 +0,0 @@
-import { utils } from "pixi.js"
-
-export class ColorHelper {
-    static generateRandomColor(): number {
-        return utils.rgb2hex([Math.random(), Math.random(), Math.random()]);
-    }
-}
