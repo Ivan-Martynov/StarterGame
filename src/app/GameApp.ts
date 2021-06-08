fbeacd142f6211c96500fb5c52630ef806cdda2e
Ivan Martynov <ivan.a.martynov@gmail.com>
2021-06-08 11:09:02 +0300

    resize app functionality

--- src/app/GameApp.ts
+++ src/app/GameApp.ts
@@ -4,11 +4,10 @@ export class GameApp {
 
     private _app: PIXI.Application;
 
-    constructor(parent: HTMLElement, width: number, height: number) {
-        const randomColor = PIXI.utils.rgb2hex([Math.random(), Math.random(), Math.random()]);
+    constructor(app: PIXI.Application) {
+        this._app = app;
+    }
 
-        this._app = new PIXI.Application({ width, height, backgroundColor: randomColor });
-
-        parent.replaceChild(this._app.view, parent.lastElementChild); // hack for parcel HMR
+    onResize(): void {
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
 import { GameApp } from "./app/GameApp";
+import * as PIXI from "pixi.js";
 
-const gameApp = new GameApp(document.body, window.innerWidth, window.innerHeight);
+// have to keep the app global
+let _app: PIXI.Application;
+
+function main(): void {
+    const randomColor = PIXI.utils.rgb2hex([Math.random(), Math.random(), Math.random()]);
+
+    _app = new PIXI.Application({
+        width: Math.max(1, window.innerWidth),
+        height: Math.max(1, window.innerHeight),
+        backgroundColor: randomColor,
+        resolution: devicePixelRatio,
+        autoDensity: true,
+    });
+
+    document.body.replaceChild(_app.view, document.body.lastElementChild); // hack for parcel HMR
+
+    window.addEventListener('resize', onResize);
+
+    const gameApp = new GameApp(_app);
+}
+
+function onResize(): void {
+    _app.renderer.resize(window.innerWidth, window.innerHeight);
+}
+
+main();
